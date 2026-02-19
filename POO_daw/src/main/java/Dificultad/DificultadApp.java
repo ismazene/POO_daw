e2f7b5a8b8ede332f7c443cb9613b2ff5f9df02d
ZENE <ismaeltomatito1449@gmail.com>
2026-02-19 17:38:00 +0100

    ++

---
+++ POO_daw/src/main/java/Dificultad/DificultadApp.java
@@ -0,0 +1,11 @@
+package Dificultad;
+
+import java.util.Scanner;
+
+public class DificultadApp {
+    static void main() {
+        Scanner teclado = new Scanner(System.in);
+
+        System.out.println("");
+    }
+}
--- POO_daw/src/main/java/pruebasEnum/Metodos.java
+++ POO_daw/src/main/java/pruebasEnum/Metodos.java
@@ -1,7 +1,11 @@
 package pruebasEnum;
 
+import java.util.Scanner;
+
 public class Metodos {
     static void main() {
+
+        Scanner teclado = new Scanner(System.in);
 
         Metodo opcion = Metodo.PAYPAL;
 
@@ -11,6 +15,12 @@ public class Metodos {
         for (Metodo metodo : Metodo.values()){
             System.out.println(metodo);
         }
+
+        /// Pedir por teclado y
+        System.out.println("Elige un metodo de pago: ");
+        Metodo op = Metodo.valueOf(teclado.next().toUpperCase());
+
+        System.out.println("Metodo elegido: " + op);
 
     }
 }
