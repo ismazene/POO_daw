738a94df7850c209605d2543441c3e72571b0700
ZENE <ismaeltomatito1449@gmail.com>
2026-04-15 16:09:48 +0200

    JAVAFX

--- demoFX/src/main/java/org/example/demofx/HelloController.java
+++ demoFX/src/main/java/org/example/demofx/HelloController.java
@@ -1,11 +1,26 @@
 package org.example.demofx;
 
 import javafx.fxml.FXML;
+import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 
 public class HelloController {
+
+    static int cont;
+
     @FXML
     private Label welcomeText;
+
+    @FXML
+    private Button contador;
+
+    @FXML
+    private Label contando;
+
+    @FXML
+    public void initialize() {
+        contador.setOnAction(e -> contando.setText(Integer.toString(++cont)));
+    }
 
     @FXML
     protected void onHelloButtonClick() {
