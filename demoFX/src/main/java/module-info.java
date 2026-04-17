f6d78819ad62a836e3e7c6c827c66b38e93e3e17
ZENE <ismaeltomatito1449@gmail.com>
2026-04-17 17:20:00 +0200

    ++

--- demoFX/src/main/java/module-info.java
+++ demoFX/src/main/java/module-info.java
@@ -10,6 +10,8 @@ module org.example.demofx {
     requires org.kordamp.bootstrapfx.core;
     requires eu.hansolo.tilesfx;
     requires com.almasb.fxgl.all;
+    requires java.desktop;
+    requires javafx.base;
 
     opens org.example.demofx to javafx.fxml;
     exports org.example.demofx;
--- demoFX/src/main/java/org/example/demofx/Pantalla2controller.java
+++ demoFX/src/main/java/org/example/demofx/Pantalla2controller.java
@@ -1,11 +1,62 @@
 package org.example.demofx;
 
+import javafx.beans.Observable;
+import javafx.beans.property.SimpleIntegerProperty;
+import javafx.beans.property.SimpleStringProperty;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
+import javafx.fxml.FXML;
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableView;
+import javafx.scene.control.TextField;
+
 import java.io.IOException;
+import java.util.ArrayList;
+
 
 public class Pantalla2controller {
+
+    @FXML
+    private TextField nombreTextField;
+
+    @FXML
+    private TextField edadTextField;
+
+    @FXML
+    private TableView<Persona> personasTableView;
+
+    @FXML
+    private TableColumn<Persona,String> nombreTableColumn;
+
+    @FXML
+    private TableColumn<Persona,Integer> edadTableColumn;
+
+    @FXML
+    public void initialize(){
+        nombreTableColumn.setCellValueFactory(datos -> new SimpleStringProperty(datos.getValue().getNombre()));
+        edadTableColumn.setCellValueFactory(datos -> new SimpleIntegerProperty(datos.getValue().getEdad()).asObject());
+
+        personasTableView.setItems(PersonasCreadas.getListaPersonas());
+
+    }
+
+
     public void volverButton() throws IOException {
 
         HelloApplication.setRoot("hello-view");
     }
 
+    public void guardarButton() {
+        String nombre = nombreTextField.getText();
+        Integer edad = Integer.parseInt(edadTextField.getText() );
+
+        Persona persona = new Persona(nombre,edad);
+        PersonasCreadas.insertarPersona(persona);
+        System.out.println("Persona creada: " + nombre + " - " + edad);
+        System.out.println(PersonasCreadas.getListaPersonas());
+
+        nombreTextField.clear();
+        edadTextField.clear();
+
+    }
 }
